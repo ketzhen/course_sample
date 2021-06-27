723279cd19dc952b6a34471a0fc42f50034cb2cf
p.anisova <p.anisova@corp.mail.ru>
2021-06-27 18:59:05 +0300

    add tests remote execution

--- config.py
+++ config.py
@@ -1,3 +1,13 @@
+from selenium import webdriver
+
 USERS = {"QA": {"login": "QA_tester_1", "password": "Tester2019!"},
          "PM": {"login": "Project_1", "password": "Projectmanager1"}}
 LINKS = {"main_page": "https://testit.geekbrains.ru/"}
+BROWSER_REMOTE_CAPABILITIES = {
+       "browserName": "chrome",
+       "version": "90.0",
+       "enableVNC": True,
+   }
+VALID_BROWSERS = {
+    "local": webdriver.Chrome,
+    "remote": webdriver.Remote}
--- conftest.py
+++ conftest.py
@@ -1,10 +1,16 @@
 import pytest
-from selenium import webdriver
+import config
 
 
 @pytest.fixture(scope="function")
-def browser():
-    browser = webdriver.Chrome()
+def browser(request):
+    launch = request.config.getoption("--launch")
+    capabilities = config.BROWSER_REMOTE_CAPABILITIES if launch == "remote" else None
+    browser = config.VALID_BROWSERS[launch](desired_capabilities=capabilities)
     browser.maximize_window()
     yield browser
     browser.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption("--launch", default="local")

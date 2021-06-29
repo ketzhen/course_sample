bceeeb88a6f7bc966e957a7bb078d00be9bc99cb
marina <hakegawa@gmail.com>
2021-06-29 11:11:14 +0300

    Add CI options

--- conftest.py
+++ conftest.py
@@ -3,8 +3,25 @@ from selenium import webdriver
 
 
 @pytest.fixture(scope="function")
-def browser():
-    browser = webdriver.Chrome()
+def browser(request):
+    browser_location = request.config.option.browser_location
+
+    if browser_location == "remote":
+        capabilities = {
+            "browserName": "chrome",
+            "selenoid:options": {
+                "enableVNC": True,
+                "enableVideo": False
+            }
+        }
+        browser = webdriver.Remote(command_executor="http://localhost:4444/wd/hub", desired_capabilities=capabilities)
+    else:
+        browser = webdriver.Chrome()
+
     browser.maximize_window()
     yield browser
     browser.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser_location")
